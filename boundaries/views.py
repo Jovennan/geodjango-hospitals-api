25fe1e519150f51c060f0d41dab07ea406ca046e
Jovennan Ramalho <jovennan@gmail.com>
2023-02-21 19:48:49 -0300

    feat: add CRUD functionality for boundary

--- boundaries/views.py
+++ boundaries/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Boundary
+from .serializers import BoundarySerializer
+
+
+class BoundaryViewSet(viewsets.ModelViewSet):
+    queryset = Boundary.objects.all()
+    serializer_class = BoundarySerializer
+
